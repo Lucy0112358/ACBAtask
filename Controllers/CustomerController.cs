846466ce62f328c6b27af19cbcba3dcc2ed60895
Lucy0112358 <lusinehsahakian@gmail.com>
2023-09-07 21:08:19 +0400

    jwt fixed

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,8 +1,12 @@
-﻿using ACBAbankTask.Entities;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ACBAbankTask.Entities;
 using ACBAbankTask.Models;
 using ACBAbankTask.Services;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ACBAbankTask.Controllers
 {
@@ -19,6 +23,91 @@ namespace ACBAbankTask.Controllers
             _configuration = configuration;
                _customerService = customerService;
         }
+
+        [HttpGet("signin")]
+        public IActionResult SignIn(string email, string password)
+        {
+            // Create a SqlConnection using the connection string from appsettings.json
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+
+                // Create a SqlCommand to execute your SQL query
+                using (var command = new SqlCommand("SELECT Id, Email, Password FROM Customers WHERE Email = @Email AND Password = @Password", connection))
+                {
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@Password", password);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            int userId = reader.GetInt32(0);
+                            string userEmail = reader.GetString(1);
+                            return Ok(GetAccessToken(userEmail, userId));
+                        }
+                        else
+                        {
+                            return Unauthorized("Invalid email or password");
+                        }
+                    }
+                }
+            }
+        }
+
+        private string GetAccessToken(string email, int id)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SigningKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken("issuer",
+              "aud",
+              new List<Claim>() {
+            new Claim(JwtRegisteredClaimNames.NameId, id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, email),
+            new Claim("role", "Admin")
+              },
+              expires: DateTime.UtcNow.AddDays(14),
+              signingCredentials: credentials
+            );
+            var accessToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return accessToken;
+        }
+
+
+        /*        [HttpGet("signin")]
+                public IActionResult SignIn(string email, string password)
+                {
+                    var users = new List<User>()
+                {
+                    new User
+                    {
+                        Id=1,
+                        Email=email,
+                        Password="pass",
+                    }
+                };
+                    var user = users.FirstOrDefault(u => u.Email == email);
+                    return Ok(GetAccessToken(user.Email, user.Id));
+                }
+
+                private string GetAccessToken(string email, int id)
+                {
+                    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SigningKey"]));
+                    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+                    var token = new JwtSecurityToken("issuer",
+                      "aud",
+                      new List<Claim>() {
+                        new Claim(JwtRegisteredClaimNames.NameId,id.ToString()),
+                        new Claim(JwtRegisteredClaimNames.Email,email),
+                        new Claim("role", "Admin")
+                      },
+                      expires: DateTime.UtcNow.AddDays(14),
+                      signingCredentials: credentials
+                      );
+                    var accessToken = new JwtSecurityTokenHandler().WriteToken(token);
+                    return accessToken;
+                }
+        */
 
         // GET: api/<CustomerController>
         [HttpGet]
--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 
+using System.Configuration;
+using System.Text;
 using ACBAbankTask.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace ACBAbankTask
 {
@@ -8,9 +13,49 @@ namespace ACBAbankTask
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.Configuration.AddJsonFile("appsettings.json");
             builder.Services.AddScoped<CustomerService>();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(builder.Configuration["Jwt:SigningKey"]))
+        };
+    });
+            builder.Services.AddAuthorization();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SpenzServerNetCore", Version = "v1" });
+                var jwtSecurityScheme = new OpenApiSecurityScheme
+                {
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Name = "JWT Authentication",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
 
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer",
+                   jwtSecurityScheme);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {jwtSecurityScheme,Array.Empty<string>() }
+                    });
+
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
